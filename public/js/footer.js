90afa9db398bfd027c442ca40eaf28ed4abe572c
naglaa77 <sama_hap@yahoo.com>
2022-04-14 15:05:04 +0100

    finish home page

---
+++ public/js/footer.js
@@ -0,0 +1,60 @@
+const creatFooter = () => {
+
+    let footer = document.querySelector('footer');
+    footer.innerHTML = `
+        <div class="footer-content">
+            <img src="img/light-logo.png" class="logo" alt="logo">
+            <div class="footer-ul-container">
+                <ul class="category">
+                    <li class="category-title">men</li>
+                    <li><a href="#" class="footer-link">t-shirts</a></li>
+                    <li><a href="#" class="footer-link">sweatshirts</a></li>
+                    <li><a href="#" class="footer-link">shirts</a></li>
+                    <li><a href="#" class="footer-link">jeans</a></li>
+                    <li><a href="#" class="footer-link">trousers</a></li>
+                    <li><a href="#" class="footer-link">shoes</a></li>
+                    <li><a href="#" class="footer-link">casuals</a></li>
+                    <li><a href="#" class="footer-link">formals</a></li>
+                    <li><a href="#" class="footer-link">sports</a></li>
+                    <li><a href="#" class="footer-link">watch</a></li>
+                </ul>
+                <ul class="category">
+                    <li class="category-title">women</li>
+                    <li><a href="#" class="footer-link">t-shirts</a></li>
+                    <li><a href="#" class="footer-link">sweatshirts</a></li>
+                    <li><a href="#" class="footer-link">shirts</a></li>
+                    <li><a href="#" class="footer-link">jeans</a></li>
+                    <li><a href="#" class="footer-link">trousers</a></li>
+                    <li><a href="#" class="footer-link">shoes</a></li>
+                    <li><a href="#" class="footer-link">casuals</a></li>
+                    <li><a href="#" class="footer-link">formals</a></li>
+                    <li><a href="#" class="footer-link">sports</a></li>
+                    <li><a href="#" class="footer-link">watch</a></li>
+                </ul>
+                
+            </div>
+    </div>
+    <p class="footer-title">about company</p>
+    <p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quisquam magni dolore esse ab aliquid accusantium provident nesciunt corrupti. Sint laudantium error, ad atque quo possimus, perferendis provident esse aliquid minus eum ipsam, quos dicta deleniti numquam suscipit natus ducimus adipisci accusantium. Delectus deleniti officiis, iusto fuga atque repellat porro tenetur sapiente odit ab voluptatem optio illo quia nihil? Provident magnam accusamus, molestiae sapiente vel suscipit modi deserunt iusto nesciunt voluptatibus veniam praesentium impedit officiis ut repudiandae laudantium earum sint. Aliquam velit reiciendis eaque laboriosam. A totam ullam praesentium doloremque commodi iste, necessitatibus sed quam fuga asperiores quo quis modi.</p>
+    <p class="info">support emails - help@colthing.com, customersupport@cloothing.com</p>
+    <p class="info">telephone - 170 00 00 001,180 00 00 002</p>
+    <div class="footer-social-container">
+        <div>
+            <a href="#" class="social-link">terms & services</a>
+            <a href="#" class="social-link">privacy page</a>
+        </div>
+        <div>
+            <a href="#" class="social-link">instagram</a>
+            <a href="#" class="social-link">facebook</a>
+            <a href="#" class="social-link">twitter</a>
+        </div>
+    </div>
+    <p class="footer-credit">
+        Closing, best apparels online store
+    </p>
+    
+    `;
+
+}
+
+creatFooter();
